a837e147d05ddd5049f75139dc1f2819cb9d58c5
augustalex <augustalexandersson@gmail.com>
2018-02-03 00:49:45 +0100

    ClientServer: Serves static resources

--- clientServer.js
+++ clientServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.sendFile('./index.html'));
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
